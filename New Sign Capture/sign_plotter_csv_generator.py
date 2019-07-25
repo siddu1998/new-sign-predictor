0d02c9722c125b753a4ab46e7fac0203f4dac409
Maram <smaram7@gatech.edu>
2019-07-25 13:53:12 -0400

    Env shift w10->linux

--- New Sign Capture/sign_plotter_csv_generator.py
+++ New Sign Capture/sign_plotter_csv_generator.py
@@ -11,41 +11,24 @@ def save_coords(event):
     print("you clicked on", click_loc)
     coords.append(click_loc)
 
-# Function to load the next image into the Label
 def next_img():
-    img_1,img_2=pairwise(imgs)
-    img_1=Image.open(img_1)
-    img_2=Image.open(img_2)
-    img_front_label_first.img = tk.PhotoImage(img_)1
-    img_front_label_first.config(image=img_front_label_first.img)
-
-    
+    img_label.img = tk.PhotoImage(file=next(imgs))
+    img_label.config(image=img_label.img)
 
 
 
 root = tk.Tk()
 
 # Choose multiple images
-img_dir = filedialog.askdirectory(parent=root, initialdir="C:/smaram7/Desktop", title='Choose folder')
+img_dir = filedialog.askdirectory(parent=root, initialdir="D:/Temp/", title='Choose folder')
 os.chdir(img_dir)
 imgs = iter(os.listdir(img_dir))
 
-img_front_label_first = tk.Label(root)
-img_front_label_second = tk.Label(root)
+img_label = tk.Label(root)
+img_label.pack()
+img_label.bind("<Button-1>",save_coords)
 
-img_front_label_first.pack()
-img_front_label_second.pack()
-
-
-
-
-
-img_front_label_first.bind("<Button-1>",save_coords)
-img_front_label_second.bind("<Button-1>",save_coords)
-
-
-
-btn = tk.Button(root, text='Next pair', command=next_img)
+btn = tk.Button(root, text='Next image', command=next_img)
 btn.pack()
 
 next_img() # load first image
